eed07beb684888828fe071d50fc316e22e636991
calvinchu8172 <calvinchu8172@gmail.com>
2015-02-05 18:13:12 +0800

    Add user :name for devise

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,9 @@
 ActiveRecord::Schema.define(version: 20150129180506) do
 
   create_table "users", force: true do |t|
+    t.string   "name"
     t.string   "email",                  default: "", null: false
+    t.string   "image"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -30,7 +32,7 @@ ActiveRecord::Schema.define(version: 20150129180506) do
     t.string   "uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
